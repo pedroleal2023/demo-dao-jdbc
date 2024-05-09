7b905fff0cfe77bcc9f40afc0044d61d741b8d9b
Pedro Miguel <rudaprod.oficial@gmail.com>
2024-05-09 20:57:37 -0300

    DepartmentDaoJDBC implements

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,18 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoImplJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao () {
 		return new SellerDaoImplJDBC(DB.getConnection());
+		
+	}
+	
+	public static DepartmentDao createDepartmentDao () {
+		return new DepartmentDaoJDBC(DB.getConnection());
 		
 	}
 }
